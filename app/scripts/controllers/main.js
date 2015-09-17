886ffc70c8d75695801a631cf0dfd6d003bbdbfc
stran <stran@redhat.com>
2015-09-17 19:48:43 -0400

    Just some random stuff I got from tutorials

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,20 @@
  * Controller of the kurtangularApp
  */
 angular.module('kurtangularApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  .controller('MainCtrl', function ($scope, $http) {
+	  this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+	  $scope.whoismyhero = "kurt angle is my hero";
+
+	  function getdata(zipcode) {
+		  if (zipcode) {
+		    $http.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipcode)
+		    .success(function(response) {$scope.data = response.weather[0].description;});
+	      }
+	  }
+	  
+	  $scope.getdata = getdata;
   });
