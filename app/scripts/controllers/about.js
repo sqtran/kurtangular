e6c9daf26e59128c2384cd0259b745b14ce6fa1e
stran <stran@redhat.com>
2015-09-17 17:44:26 -0400

    Initial Commit of Kurt Angular Project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kurtangularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kurtangularApp
+ */
+angular.module('kurtangularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
